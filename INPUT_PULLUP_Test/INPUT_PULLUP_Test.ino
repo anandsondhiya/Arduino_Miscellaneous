bb85a7912bb7b2250ff81a32d721f09b9b555d0d
Anand Sondhiya <anandsondhiya09@gmail.com>
2023-02-21 21:04:45 +0530

    Create INPUT_PULLUP_Test.ino

---
+++ INPUT_PULLUP_Test/INPUT_PULLUP_Test.ino
@@ -0,0 +1,13 @@
+#define button1 13
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(button1, INPUT);
+}
+
+void loop() {
+  int ButtonState = digitalRead(button1);
+
+  Serial.println(ButtonState);
+
+}
